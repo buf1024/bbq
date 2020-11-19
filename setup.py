4e1f7fb7669ba47d24a302ad709f9213bd18f74d
luoguochun <buf1024@gmail.com>
2020-11-19 14:50:37 +0800

    修复BUG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bbq',
-    version='1.0.0',
+    version='0.0.1',
     packages=find_packages(include=['bbq']),
     include_package_data=True,
     zip_safe=False,
